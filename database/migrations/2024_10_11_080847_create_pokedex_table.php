419621a94380dcd09f6432a769c3a561ee1d5f1f
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2024-10-11 15:29:29 +0700

    CODE: Add database migration

---
+++ database/migrations/2024_10_11_080847_create_pokedex_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pokedex', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('species');
+            $table->string('primary_type');
+            $table->decimal('weight', 8, 2)->default(0.0);
+            $table->decimal('height', 8, 2)->default(0.0);
+            $table->integer('hp')->default(0);
+            $table->integer('attack')->default(0);
+            $table->integer('defense')->default(0);
+            $table->boolean('is_legendary')->default(false);
+            $table->string('photo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pokedex');
+    }
+};

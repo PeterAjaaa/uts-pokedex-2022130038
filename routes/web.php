40ba181a81ba8fa3fdaf04e34d8cbce9bd486f2c
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2024-10-11 17:15:40 +0700

    CODE: Add views, models, and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/create', function () {
+    return view('create');
 });

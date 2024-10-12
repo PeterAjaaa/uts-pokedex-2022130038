8b87ef2fa9e9aceea999230741c65562effeddda
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2024-10-12 20:45:48 +0700

    REQ: Change index view to fit into requirements

--- resources/views/pokemon/index.blade.php
+++ resources/views/pokemon/index.blade.php
@@ -20,12 +20,7 @@
                             <th scope="col" class="text-center">Name</th>
                             <th scope="col" class="text-center">Species</th>
                             <th scope="col" class="text-center">Primary Type</th>
-                            <th scope="col" class="text-center">Weight</th>
-                            <th scope="col" class="text-center">Height</th>
-                            <th scope="col" class="text-center">HP</th>
-                            <th scope="col" class="text-center">Attack</th>
-                            <th scope="col" class="text-center">Defense</th>
-                            <th scope="col" class="text-center">Legendary Status</th>
+                            <th scope="col" class="text-center">Power</th>
                             <th scope="col" class="text-center">Image</th>
                             <th scope="col" class="text-center">Actions</th>
                         </tr>
@@ -33,7 +28,7 @@
                     <tbody>
                         @foreach ($pokemons as $pokemon)
                             <tr>
-                                <td class="text-center">{{ $pokemon->id }}</td>
+                                <td class="text-center">{{ '#' . Str::padleft($pokemon->id, 4, 0) }}</td>
                                 <td class="text-center">
                                     <a href="{{ route('pokemon.show', $pokemon) }}">
                                         {{ $pokemon->name }}
@@ -41,28 +36,19 @@
                                 </td>
                                 <td class="text-center">{{ $pokemon->species }}</td>
                                 <td class="text-center">{{ $pokemon->primary_type }}</td>
-                                <td class="text-center">{{ $pokemon->weight }}</td>
-                                <td class="text-center">{{ $pokemon->height }}</td>
-                                <td class="text-center">{{ $pokemon->hp }}</td>
-                                <td class="text-center">{{ $pokemon->attack }}</td>
-                                <td class="text-center">{{ $pokemon->defense }}</td>
-                                @if ($pokemon->is_legendary)
-                                    <td class="text-center">Yes</td>
-                                @else
-                                    <td class="text-center">No</td>
-                                @endif
+                                <td class="text-center">{{ $pokemon->hp + $pokemon->attack + $pokemon->defense }}</td>
                                 <td class="text-center">
                                     <img src="{{ Storage::url($pokemon->photo) }}" class="img-thumbnail w-50">
                                 </td>
                                 <td>
                                     <div class="btn-group" role="group">
-                                        <a class="btn btn-warning" href="{{ route('pokemon.edit', $pokemon) }}">
+                                        <a class="btn btn-warning mt-3 me-3" href="{{ route('pokemon.edit', $pokemon) }}">
                                             Edit
                                         </a>
                                         <form action="{{ route('pokemon.destroy', $pokemon) }}" method="POST">
                                             @csrf
                                             @method('delete')
-                                            <button class="btn btn-danger" type="submit">Delete</button>
+                                            <button class="btn btn-danger mt-3 me-3" type="submit">Delete</button>
                                         </form>
                                     </div>
                                 </td>

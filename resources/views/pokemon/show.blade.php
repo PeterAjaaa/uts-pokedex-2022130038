964fc1acb38ad9760a6f90fda43ea0e42769049f
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2024-10-12 20:23:43 +0700

    CODE: Add placeholder for detailed pokemon view

--- resources/views/pokemon/show.blade.php
+++ resources/views/pokemon/show.blade.php
@@ -2,8 +2,32 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-left">
+        <main>
+            <h1>{{ $pokemon->name }}</h1>
+            <p>
+                {{ $pokemon->species }}
+            </p>
 
-        </div>
+            <table class="table table-primary">
+                <tbody>
+                    <tr>
+                        <td><b>Retail Price</b></td>
+                        <td>{{ $pokemon->retail_price }}</td>
+                    </tr>
+                    <tr>
+                        <td><b>Wholesale Price</b></td>
+                        <td>{{ $pokemon->wholesale_price }}</td>
+                    </tr>
+                    <tr>
+                        <td><b>Min Wholesale Qty.</b></td>
+                        <td>{{ $pokemon->min_wholesale_qty }}</td>
+                    </tr>
+                    <tr>
+                        <td><b>Quantity</b></td>
+                        <td>{{ $pokemon->quantity }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </main>
     </div>
 @endsection
